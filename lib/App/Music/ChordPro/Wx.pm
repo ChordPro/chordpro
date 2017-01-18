ba204a9075f721ce534c009826ab04b563699ad5
Johan Vromans <jvromans@squirrel.nl>
2017-01-18 20:04:53 +0100

    Update WxChordPro to 0.701.2.

---
+++ lib/App/Music/ChordPro/Wx.pm
@@ -0,0 +1,44 @@
+#! perl
+
+use strict;
+use warnings;
+use utf8;
+
+package App::Music::ChordPro::Wx;
+
+our $VERSION = "0.710.2";
+
+1;
+
+=head1 NAME
+
+wxchordpro - a simple Wx-based GUI wrapper for ChordPro
+
+=head1 SYNOPSIS
+
+  wxchordpro [ ChordPro file ]
+
+=head1 DESCRIPTION
+
+B<wxchordpro> is a GUI wrapper for the ChordPro program. It allows
+opening of files, make changes, and preview (optionally print) the
+formatted result.
+
+For more information about the ChordPro file format, see
+L<http://www.chordpro.org>.
+
+For more information about ChordPro program, see L<App::Music::ChordPro>.
+
+=head1 LICENSE
+
+Copyright (C) 2010,2017 Johan Vromans,
+
+This module is free software. You can redistribute it and/or
+modify it under the terms of the Artistic License 2.0.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
--- script/wxchordpro.pl
+++ script/wxchordpro.pl
@@ -23,6 +23,11 @@ sub OnInit {
 
     my $main = App::Music::ChordPro::Wx::Main->new();
     exit unless $main->init;
+
+#    my $icon = Wx::Icon->new();
+#    $icon->CopyFromBitmap(Wx::Bitmap->new("wxchordpro.jpg", wxBITMAP_TYPE_ANY));
+#    $main->SetIcon($icon);
+
     $self->SetTopWindow($main);
     $main->Show(1);
 
@@ -35,3 +40,36 @@ sub OnInit {
 
 my $m = main->new();
 $m->MainLoop();
+
+=head1 NAME
+
+wxchordpro - a simple Wx-based GUI wrapper for ChordPro
+
+=head1 SYNOPSIS
+
+  wxchordpro [ file ]
+
+=head1 DESCRIPTION
+
+B<wxchordpro> is a GUI wrapper for the ChordPro program. It allows
+opening of files, make changes, and preview (optionally print) the
+formatted result.
+
+For more information about the ChordPro file format, see
+L<http://www.chordpro.org>.
+
+For more information about ChordPro program, see L<App::Music::ChordPro>.
+
+=head1 LICENSE
+
+Copyright (C) 2010,2017 Johan Vromans,
+
+This module is free software. You can redistribute it and/or
+modify it under the terms of the Artistic License 2.0.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
