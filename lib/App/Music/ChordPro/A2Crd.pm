8d8b82a444bc0d1bbdb0bfd08b07ddafb5c3c935
Johan Vromans <jvromans@squirrel.nl>
2018-10-29 21:03:57 +0100

    Add (initial version of) a2crd script.

---
+++ lib/App/Music/ChordPro/A2Crd.pm
@@ -0,0 +1,110 @@
+#! perl
+
+use strict;
+use warnings;
+use utf8;
+use Carp;
+
+package App::Music::ChordPro::A2Crd;
+
+################ Subroutines ################
+
+# API: Produce ChordPro data from AsciiCRD lines.
+sub a2cho {
+    my ( $lines, $options ) = @_;
+    my $map = "";
+    foreach ( @$lines ) {
+	$map .= classify($_);
+    }
+    maplines( $map, $lines );
+}
+
+# Classify the line and return a single-char token.
+sub classify {
+    my ( $line ) = @_;
+    return '_' if /^\s*$/;	# empty line
+    return '{' if /^\{.+/;	# directive
+
+    # Lyrics or Chords heuristic.
+    my $len = length($line);
+    $line =~ s/\s+//g;
+    return ( $len / length($line) - 1 ) < 1 ? 'l' : 'c';
+}
+
+# Process the lines via the map.
+sub maplines {
+    my ( $map, $lines ) = @_;
+    my @out;
+
+    # Preamble.
+    while ( $map =~ s/^([l_{])// ) {
+	push( @out, ($1 eq "l" ? "# " : "" ) . shift( @$lines ) );
+    }
+
+    # Process the lines using the map.
+    while ( $map ) {
+	# warn($map);
+
+	# Blank line preceding chords: pass.
+	if ( $map =~ s/^_c/c/ ) {
+	    push( @out, '');
+	    shift(@$lines);
+	    # Fall through.
+	}
+
+	# The normal case: chords + lyrics.
+	if ( $map =~ s/^cl// ) {
+	    push( @out, combine( shift(@$lines), shift(@$lines), "cl" ) );
+	}
+
+	# Empty line preceding a chordless lyrics line.
+	elsif ( $map =~ s/^__l// ) {
+	    push( @out, '' );
+	    shift( @$lines );
+	    push( @out, combine( shift(@$lines), shift(@$lines), "__l" ) );
+	}
+
+	# Chordless lyrics line.
+	elsif ( $map =~ s/^_l// ) {
+	    push( @out, combine( shift(@$lines), shift(@$lines), "_l" ) );
+	}
+
+	# Lone lyrics or directives.
+	elsif ( $map =~ s/^[l{]// ) {
+	    push( @out, shift( @$lines ) );
+	}
+
+	# Lone chords.
+	elsif ( $map =~ s/^c// ) {
+	    push( @out, combine( shift(@$lines), '', "c" ) );
+	}
+
+	# Empty line.
+	elsif ( $map =~ s/^_// ) {
+	    push( @out, '' );
+	    shift( @$lines );
+	}
+
+	# Can't happen.
+	else {
+	    croak("MAP: $map");
+	}
+    }
+    return wantarray ? @out : \@out;
+}
+
+# Combine two lines (chords + lyrics) into lyrics with [chords].
+sub combine {
+    my ( $l1, $l2 ) = @_;
+    my $res = "";
+    while ( $l1 =~ /^(\s*)(\S+)(.*)/ ) {
+	$res .= join( '',
+		      substr( $l2, 0, length($1), '' ),
+		      '[' . $2 . ']',
+		      substr( $l2, 0, length($2), '' ) );
+	$l1 = $3;
+    }
+    return $res.$l2;
+}
+
+1;
---
+++ script/a2crd.pl
@@ -0,0 +1,163 @@
+#!/usr/bin/perl
+
+# A2ChordPro -- perl version of Chord/A2Crd
+
+# Author          : Johan Vromans
+# Created On      : Mon Oct 29 10:45:24 2018
+# Last Modified By: Johan Vromans
+# Last Modified On: Mon Oct 29 21:02:31 2018
+# Update Count    : 32
+# Status          : Unknown, Use with caution!
+
+################ Common stuff ################
+
+=head1 NAME
+
+a2crd - Convert ASCII Crd to ChordPro
+
+=head1 DESCRIPTION
+
+B<a2crd> will read a text file containing the lyrics of one or many
+songs with chord information written visually above the lyrics. This
+is often referred to as I<crd> data. B<a2crd> will then generate
+equivalent ChordPro output.
+
+Note that the output will generally need some additional editing to be
+useful as input to ChordPro.
+
+B<a2crd> is a wrapper around L<App::Music::ChordPro::A2Crd>, which
+does all of the work.
+
+=cut
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../CPAN";
+use lib "$FindBin::Bin/../lib";
+use App::Packager qw( :name App::Music::ChordPro );
+
+use App::Music::ChordPro::A2Crd;
+use File::LoadLines;
+
+# Package or program libraries, if appropriate.
+# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
+# use lib qw($LIBDIR);
+# require 'common.pl';
+
+# Package name.
+my $my_package = 'ChordPro';
+# Program name and version.
+my ($my_name, $my_version) = qw( a2crd 0.01 );
+
+################ Command line parameters ################
+
+use Getopt::Long 2.13;
+
+# Command line options.
+my %options;
+my $verbose = 0;		# verbose processing
+my $output;
+
+# Development options (not shown with -help).
+my $debug = 0;			# debugging
+my $trace = 0;			# trace (show process)
+my $test = 0;			# test mode.
+
+# Process command line options.
+app_options();
+
+# Post-processing.
+$trace |= ($debug || $test);
+
+$options{output} = $output;
+$options{verbose} = $verbose;
+
+################ Presets ################
+
+my $TMPDIR = $ENV{TMPDIR} || $ENV{TEMP} || '/usr/tmp';
+
+################ The Process ################
+
+binmode( STDERR, ':utf8' );
+
+my $lines = loadlines( @ARGV ? $ARGV[0] : \*DATA);
+
+my $fd;
+if ( $output && $output ne '-' ) {
+    open( $fd, '>:utf8', $output )
+      or croak("$output: $!\n");
+}
+else {
+    binmode( STDOUT, ':utf8' );
+    $fd = \*STDOUT;
+}
+
+print $fd "$_\n"
+  foreach App::Music::ChordPro::A2Crd::a2cho($lines, \%options);
+
+################ Subroutines ################
+
+sub app_options {
+    my $help = 0;		# handled locally
+    my $ident = 0;		# handled locally
+
+    # Process options, if any.
+    # Make sure defaults are set before returning!
+    return unless @ARGV > 0;
+
+    if ( !GetOptions(
+		     'output=s' => \$output,
+		     'ident'	=> \$ident,
+		     'verbose'	=> \$verbose,
+		     'trace'	=> \$trace,
+		     'help|?'	=> \$help,
+		     'debug'	=> \$debug,
+		    ) or $help )
+    {
+	app_usage(2);
+    }
+    app_ident() if $ident;
+}
+
+sub app_ident {
+    print STDERR ("This is $my_package [$my_name $my_version]\n");
+}
+
+sub app_usage {
+    my ($exit) = @_;
+    app_ident();
+    print STDERR <<EndOfUsage;
+Usage: $0 [options] [file ...]
+    --output=XXX	output file (stdout)
+    --help		this message
+    --ident		show identification
+    --verbose		verbose information
+EndOfUsage
+    exit $exit if defined $exit && $exit != 0;
+}
+
+__DATA__
+Title: Swing Low Sweet Chariot
+
+{soc}
+      D          G    D
+Swing low, sweet chariot,
+                       A7
+Comin’ for to carry me home.
+      D7         G    D
+Swing low, sweet chariot,
+              A7       D
+Comin’ for to carry me home.
+{eoc}
+
+  D                       G          D
+I looked over Jordan, and what did I see,
+                       A7
+Comin’ for to carry me home.
+  D              G            D
+A band of angels comin’ after me,
+              A7       D
+Comin’ for to carry me home.
+
