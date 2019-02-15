d6ee8605a6759c507150d809de1ec586bca7ae66
Johan Vromans <jvromans@squirrel.nl>
2019-02-15 10:09:36 +0100

    (PDF) Move diagrams drawing to separate module.

--- lib/App/Music/ChordPro/Bundle.pm
+++ lib/App/Music/ChordPro/Bundle.pm
@@ -4,6 +4,7 @@
 
 use App::Music::ChordPro::Output::Common;
 use App::Music::ChordPro::Output::PDF;
+use App::Music::ChordPro::Output::PDF::StringDiagrams;
 use App::Music::ChordPro::Output::Debug;
 use App::Music::ChordPro::Output::ChordPro;
 use App::Music::ChordPro::Output::Text;
