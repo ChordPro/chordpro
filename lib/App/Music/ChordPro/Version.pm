70a8ae6d0050569f2099b008fd32f8f6af886124
Johan Vromans <jvromans@squirrel.nl>
2019-08-19 13:05:38 +0200

    Remove unneeded files.

--- lib/App/Music/ChordPro/Version.pm
+++ lib/App/Music/ChordPro/Version.pm
@@ -1,3 +1,7 @@
 # This file is generated. Do not edit!
 package App::Music::ChordPro::Version;
+<<<<<<< HEAD
 our $VERSION = "0.973_009";
+=======
+our $VERSION = "0.973_012";
+>>>>>>> 3fb25cd... Remove unneeded files.
