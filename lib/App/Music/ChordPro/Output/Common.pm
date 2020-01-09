cb9a80793d2a4605e89d72d364a07a2e6add6e19
Johan Vromans <jvromans@squirrel.nl>
2020-01-09 23:21:45 +0100

    (Output/Common) Add Roman conversion.

--- lib/App/Music/ChordPro/Output/Common.pm
+++ lib/App/Music/ChordPro/Output/Common.pm
@@ -28,4 +28,68 @@ sub fmt_subst {
 		 $t );
 }
 
+# Roman - functions for converting between Roman and Arabic numerals
+# 
+# Stolen from Roman Version 1.24 by OZAWA Sakuro <ozawa at aisoft.co.jp>
+# 1995-1997 and Alexandr Ciornii, C<< <alexchorny at gmail.com> >> 2007
+# 
+# Copyright (c) 1995 OZAWA Sakuro.  All rights reserved.  This program
+# is free software; you can redistribute it and/or modify it under the
+# same terms as Perl itself.
+
+our %roman2arabic = qw(I 1 V 5 X 10 L 50 C 100 D 500 M 1000);
+my %roman_digit = qw(1 IV 10 XL 100 CD 1000 MMMMMM);
+my @figure = reverse sort keys %roman_digit;
+#my %roman_digit;
+$roman_digit{$_} = [split(//, $roman_digit{$_}, 2)] foreach @figure;
+
+sub isroman($) {
+    my $arg = shift;
+    $arg ne '' and
+      $arg =~ /^(?: M{0,3})
+                (?: D?C{0,3} | C[DM])
+                (?: L?X{0,3} | X[LC])
+                (?: V?I{0,3} | I[VX])$/ix;
+}
+
+sub arabic($) {
+    my $arg = shift;
+    isroman $arg or return undef;
+    my($last_digit) = 1000;
+    my($arabic);
+    foreach (split(//, uc $arg)) {
+        my($digit) = $roman2arabic{$_};
+        $arabic -= 2 * $last_digit if $last_digit < $digit;
+        $arabic += ($last_digit = $digit);
+    }
+    $arabic;
+}
+
+sub Roman($) {
+    my $arg = shift;
+    0 < $arg and $arg < 4000 or return undef;
+    my($x, $roman);
+    foreach (@figure) {
+        my($digit, $i, $v) = (int($arg / $_), @{$roman_digit{$_}});
+        if (1 <= $digit and $digit <= 3) {
+            $roman .= $i x $digit;
+        } elsif ($digit == 4) {
+            $roman .= "$i$v";
+        } elsif ($digit == 5) {
+            $roman .= $v;
+        } elsif (6 <= $digit and $digit <= 8) {
+            $roman .= $v . $i x ($digit - 5);
+        } elsif ($digit == 9) {
+            $roman .= "$i$x";
+        }
+        $arg -= $digit * $_;
+        $x = $i;
+    }
+    $roman;
+}
+
+sub roman($) {
+    lc Roman shift;
+}
+
 1;
