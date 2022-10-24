6e54206bec7443e2dbe2cecbd906a66622483c71
Johan Vromans <jvromans@squirrel.nl>
2022-10-24 22:50:47 +0200

    Fix page labels for PDF::API2 >= 2.041.

--- lib/App/Music/ChordPro/Output/PDF/Writer.pm
+++ lib/App/Music/ChordPro/Output/PDF/Writer.pm
@@ -430,7 +430,7 @@ sub pagelabel {
                               $style eq 'alpha' ? 'a' : 'D',
 		     defined $prefix ? ( prefix => $prefix ) : (),
 		     start => 1 };
-	$c->( $self->{pdf}, $page, $opts );
+	$c->( $self->{pdf}, $page, %$opts );
     }
     else {
 	my $opts = { -style => $style,
--- lib/App/Music/ChordPro/Version.pm
+++ lib/App/Music/ChordPro/Version.pm
@@ -1,4 +1,4 @@
 # This file is generated. Do not edit!
 package App::Music::ChordPro::Version;
-our $VERSION = "5.989_004";
+our $VERSION = "5.989_005";
 print "$VERSION\n" unless caller;
