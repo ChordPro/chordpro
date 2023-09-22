c8bdee783c63c6515b33999e5c35dc36a2dbed6d
Johan Vromans <jvromans@squirrel.nl>
2023-09-22 20:38:04 +0200

    (PDF) Prevent case problems when looking up fonts for SVG.

--- lib/ChordPro/Output/PDF.pm
+++ lib/ChordPro/Output/PDF.pm
@@ -3092,10 +3092,10 @@ sub is_corefont {
 sub svg_fonthandler {
     my ( $ps, $el, $pdf, $style ) = @_;
 
-    my $family = $style->{'font-family'};
-    my $stl    = $style->{'font-style'}   // "normal";
-    my $weight = $style->{'font-weight'}  // "normal";
-    my $size   = $style->{'font-size'}    || 12;
+    my $family = lc $style->{'font-family'};
+    my $stl    = lc $style->{'font-style'}   // "normal";
+    my $weight = lc $style->{'font-weight'}  // "normal";
+    my $size   = $style->{'font-size'}       || 12;
     my $key    = join( "|", $family, $stl, $weight );
     state $fc  = {};
 
