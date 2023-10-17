3a1946b22c6fc6c5f742b3dce1e3c3ab9b59d52c
Johan Vromans <jvromans@squirrel.nl>
2023-10-17 07:31:01 +0200

    Update manifests.

--- pp/common/Text_Layout_Bundle.pm
+++ pp/common/Text_Layout_Bundle.pm
@@ -5,4 +5,5 @@ use Text::Layout::FontConfig;
 use Text::Layout::FontDescriptor;
 use Text::Layout::PDFAPI2;
 use Text::Layout::PDFAPI2::ImageElement;
+use Text::Layout::ElementRole;
 use Text::Layout::Version;
