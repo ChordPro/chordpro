bd39ede3b8bc5ee6695e8e4ae1632fe914a10317
Johan Vromans <jvromans@squirrel.nl>
2024-10-25 22:13:12 +0200

    (Wx) Force include Wx modules.

--- pp/common/wxchordpro.pp
+++ pp/common/wxchordpro.pp
@@ -3,6 +3,22 @@
 @chordpro.pp
 --cachedeps=wxchordpro.pp.deps
 
+# Explicitly include the Wx modules.
+--module=ChordPro::Wx::Config
+--module=ChordPro::Wx::EditorPanel
+--module=ChordPro::Wx::EditorPanel_wxg
+--module=ChordPro::Wx::Main
+--module=ChordPro::Wx::Main_wxg
+--module=ChordPro::Wx::PanelRole
+--module=ChordPro::Wx::PreferencesDialog
+--module=ChordPro::Wx::PreferencesDialog_wxg
+--module=ChordPro::Wx::Preview
+--module=ChordPro::Wx::RenderDialog
+--module=ChordPro::Wx::RenderDialog_wxg
+--module=ChordPro::Wx::SongbookExportPanel
+--module=ChordPro::Wx::SongbookExportPanel_wxg
+--module=ChordPro::Wx::Utils
+
 # Explicitly include the demand-loaded Wx modules.
 --module=Wx::AUI
 --module=Wx::Calendar
