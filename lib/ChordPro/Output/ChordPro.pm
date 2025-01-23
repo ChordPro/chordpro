9347f8cdf494a7a77e0e37d1ac3b9049a9ee848d
Johan Vromans <jvromans@squirrel.nl>
2025-01-23 10:03:02 +0100

    (ChordPro) Handle labels for MSPro.

--- lib/ChordPro/Output/ChordPro.pm
+++ lib/ChordPro/Output/ChordPro.pm
@@ -182,7 +182,14 @@ sub generate_song ( $s ) {
 		    }
 		    elsif ( $elt->{name} eq "label" ) {
 			my $tag = $elt->{value};
-			$t .= ": " . $tag if $tag ne "";
+			if ( $variant eq "msp" ) {
+			    $tag =~ s/\s+/ /g;
+			    $t .= ": " . $tag if $tag ne "";
+			}
+			else {
+			    $tag =~ s/\n/\\n/g;
+			    $t .= " label=\"" . $tag . "\"" if $tag ne "";
+			}
 		    }
 
 		}
@@ -190,7 +197,7 @@ sub generate_song ( $s ) {
 		push( @s, $t );
 
 		if ( $ctx =~ /^abc$/ ) {
-		    if ( $elt->{id} && $variant eq "msp" ) {
+		    if ( $elt->{id} ) {
 			push( @s, @{$s->{assets}->{$elt->{id}}->{data}} );
 			next;
 		    }
