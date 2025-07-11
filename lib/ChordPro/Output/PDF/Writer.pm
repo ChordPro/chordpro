d39700de54705eb9d6c23f2c961d36c71ad84d46
Johan Vromans <jvromans@squirrel.nl>
2025-07-11 23:08:10 +0200

    (PDF/Writer) Fix date fix for PDF::API2.

--- lib/ChordPro/Output/PDF/Writer.pm
+++ lib/ChordPro/Output/PDF/Writer.pm
@@ -47,8 +47,9 @@ sub new {
 	my $apiversion = ${$pdfapi . '::VERSION'};
 	no warnings 'redefine';
 
-	# This is supposed to have been fixed but still croaks...?
-	*{$pdfapi . '::_is_date'} = sub { 1 };
+	# Fix date validation.
+	*{$pdfapi . '::_is_date'} = sub { 1 }
+	  if $apiversion < 2.045;
 
 	# Enhanced version that allows named destinations.
 	eval "use $pdfapi" . "::Annotation";
