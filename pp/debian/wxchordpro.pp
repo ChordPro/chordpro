b83920004e2708ba432188c1e08bccf154ac7710
Johan Vromans <jvromans@squirrel.nl>
2025-09-19 21:13:47 +0200

    (debian) Update Wx to 3.2.

--- pp/debian/wxchordpro.pp
+++ pp/debian/wxchordpro.pp
@@ -3,17 +3,17 @@
 @../common/wxchordpro.pp
 
 # Explicitly link the wxGTK3 libraries.
---link=libwx_baseu-3.0.so.0
---link=libwx_baseu_net-3.0.so.0
---link=libwx_baseu_xml-3.0.so.0
---link=libwx_gtk3u_adv-3.0.so.0
---link=libwx_gtk3u_aui-3.0.so.0
---link=libwx_gtk3u_core-3.0.so.0
---link=libwx_gtk3u_html-3.0.so.0
---link=libwx_gtk3u_media-3.0.so.0
---link=libwx_gtk3u_propgrid-3.0.so.0
---link=libwx_gtk3u_ribbon-3.0.so.0
---link=libwx_gtk3u_richtext-3.0.so.0
+--link=libwx_baseu-3.2.so.0
+--link=libwx_baseu_net-3.2.so.0
+--link=libwx_baseu_xml-3.2.so.0
+--link=libwx_gtk3u_adv-3.2.so.0
+--link=libwx_gtk3u_aui-3.2.so.0
+--link=libwx_gtk3u_core-3.2.so.0
+--link=libwx_gtk3u_html-3.2.so.0
+--link=libwx_gtk3u_media-3.2.so.0
+--link=libwx_gtk3u_propgrid-3.2.so.0
+--link=libwx_gtk3u_ribbon-3.2.so.0
+--link=libwx_gtk3u_richtext-3.2.so.0
 
 # And more...
 --link=libdeflate.so.0
